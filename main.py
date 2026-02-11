786e78451ecedea9af1e4f525459fd03c5707084
Peter Standing <44238507+PeterStanding@users.noreply.github.com>
2026-02-11 14:22:55 +0000

    Added tracking for All 5 Fingers
    Now Capable of tracking all 5 fingers individually
    And Coloured all of them differently

--- main.py
+++ main.py
@@ -28,11 +28,29 @@ while cap.isOpened():
 
     if results.hand_landmarks:
         for hand_landmarks in results.hand_landmarks:
+            thumb_tip = hand_landmarks[4]
             index_tip = hand_landmarks[8]
+            middle_tip = hand_landmarks[12]            
+            ring_tip = hand_landmarks[16]
+            pinky_tip = hand_landmarks[20]
+            # cv2.circle(img, center, radius, color, thickness=None, lineType=None, shift=None)
+            # Thumb
+            x,y = int(thumb_tip.x * w), int(thumb_tip.y * h)
+            cv2.circle(frame,(x,y),5,(255,0,0), -1)
+            # Index Finger
             x,y = int(index_tip.x * w), int(index_tip.y * h)
-            cv2.circle(frame,(x,y),10,(255,0,255), -1)
+            cv2.circle(frame,(x,y),5,(0,255,0), -1)
+            # Middle Finger
+            x,y = int(middle_tip.x * w), int(middle_tip.y * h)
+            cv2.circle(frame,(x,y),5,(0,0,255), -1)
+            # Ring Finger
+            x,y = int(ring_tip.x * w), int(ring_tip.y * h)
+            cv2.circle(frame,(x,y),5,(255,0,255), -1)
+            # Pinky Finger
+            x,y = int(pinky_tip.x * w), int(pinky_tip.y * h)
+            cv2.circle(frame,(x,y),5,(255,255,0), -1)
 
-    cv2.imshow("Index Finger track", frame)      
+    cv2.imshow("Finger tips", frame)      
 
     #quitting
     if cv2.waitKey(1) & 0xFF == ord('q'):
