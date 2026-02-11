1191b5316ccab1c0cf57a371178446a7587240c4
Peter Standing <44238507+PeterStanding@users.noreply.github.com>
2026-02-11 15:56:06 +0000

    Added In Right and Left Detection
    Added a Variable to change the colours based on if the hand is Right or Left
    Right = Red
    Left = Blue

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from mediapipe.tasks.python import vision
 
 base_options = python.BaseOptions(model_asset_path = 'hand_landmarker.task')
 options = vision.HandLandmarkerOptions(base_options = base_options,
-                                       num_hands = 1,
+                                       num_hands = 2,
                                        min_hand_detection_confidence = 0.8,
                                        min_hand_presence_confidence = 0.8,
                                        min_tracking_confidence = 0.8)
@@ -26,6 +26,8 @@ while cap.isOpened():
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data = rgb_frame)
     results = detector.detect(mp_image)
 
+    r = g = b = 0
+
     if results.hand_landmarks:
         for hand_landmarks in results.hand_landmarks:
             thumb_tip = hand_landmarks[4]
@@ -34,21 +36,34 @@ while cap.isOpened():
             ring_tip = hand_landmarks[16]
             pinky_tip = hand_landmarks[20]
             # cv2.circle(img, center, radius, color, thickness=None, lineType=None, shift=None)
+
+            #Changing the Colour based on the hand
+            right = (hand_landmarks[5].x>hand_landmarks[17].x)
+            if right:   # RED for Right Hand
+                r = 255
+                g = 0
+                b = 0
+            else:       # Blue for Left Hand
+                r = 0
+                g = 0
+                b = 255
+
+            # Drawing Dots on the Tips for the Fingers
             # Thumb
             x,y = int(thumb_tip.x * w), int(thumb_tip.y * h)
-            cv2.circle(frame,(x,y),5,(255,0,0), -1)
+            cv2.circle(frame,(x,y),5,(r,g,b), -1)
             # Index Finger
             x,y = int(index_tip.x * w), int(index_tip.y * h)
-            cv2.circle(frame,(x,y),5,(0,255,0), -1)
+            cv2.circle(frame,(x,y),5,(r,g,b), -1)
             # Middle Finger
             x,y = int(middle_tip.x * w), int(middle_tip.y * h)
-            cv2.circle(frame,(x,y),5,(0,0,255), -1)
+            cv2.circle(frame,(x,y),5,(r,g,b), -1)
             # Ring Finger
             x,y = int(ring_tip.x * w), int(ring_tip.y * h)
-            cv2.circle(frame,(x,y),5,(255,0,255), -1)
+            cv2.circle(frame,(x,y),5,(r,g,b), -1)
             # Pinky Finger
             x,y = int(pinky_tip.x * w), int(pinky_tip.y * h)
-            cv2.circle(frame,(x,y),5,(255,255,0), -1)
+            cv2.circle(frame,(x,y),5,(r,g,b), -1)
 
     cv2.imshow("Finger tips", frame)      
 
